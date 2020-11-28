4137a56b2a7f580221ea93b637188f6e696d8efb
AlexisAbalea <abaleaalexis@gmail.com>
2020-11-28 18:03:50 +0100

    watib

--- js/replace.js
+++ js/replace.js
@@ -1,77 +1,69 @@
 // vim:set sw=2 ts=2 sts=2 expandtab:
 const replace = [
-  {
-    "name": " un ",
-    "replaceWith": " un wati "
-  },
-  {
-    "name": " une ",
-    "replaceWith": " une wati "
-  },
-  {
-    "name": " ce ",
-    "replaceWith": " ce wati "
-  },
-  {
-    "name": " ces ",
-    "replaceWith": " ces watis "
-  },
-  {
-    "name": " les ",
-    "replaceWith": " les watis "
-  },
-  {
-    "name": " le ",
-    "replaceWith": " le wati "
-  },
-  {
-    "name": " la ",
-    "replaceWith": " la wati "
-  },
-  {
-    "name": " mon ",
-    "replaceWith": " mon wati "
-  },
-  {
-    "name": " ma ",
-    "replaceWith": " ma wati "
-  },
-  {
-    "name": " mes ",
-    "replaceWith": " mes watis ",
-  },
-  {
-    "name": " des ",
-    "replaceWith": " des watis "
-  },
-  {
-    "name": " vos ",
-    "replaceWith": " vos watis "
-  },
-  {
-    "name": " ton ",
-    "replaceWith": " ton wati "
-  },
-  {
-    "name": " son ",
-    "replaceWith": " son wati "
-  },
-  {
-    "name": " sa ",
-    "replaceWith": " sa wati "
-  },
-  {
-    "name": " ses ",
-    "replaceWith": " ses watis "
-  },
-  {
-    "name": " c'est ",
-    "replaceWith": " c'est wati "
-  },
-  {
-    "name": " du ",
-    "replaceWith": " du wati "
-  }
+  // définis
+  {"name": " le ", "replaceWith": " le wati "},
+  {"name": " la ", "replaceWith": " la wati "},
+  {"name": " les ", "replaceWith": " les watis "},
+
+  {"name": "Le ", "replaceWith": "Le wati "},
+  {"name": "La ", "replaceWith": "La wati "},
+  {"name": "Les ", "replaceWith": "Les watis "},
+  // indéfinis
+  {"name": " un ", "replaceWith": " un wati "},
+  {"name": " une ", "replaceWith": " une wati "},
+  {"name": " des ", "replaceWith": " des watis "},
+
+  {"name": "Un ", "replaceWith": "Un wati "},
+  {"name": "Une ", "replaceWith": "Une wati "},
+  {"name": "Des ", "replaceWith": "Des watis "},
+  // possessifs
+  {"name": " mon ", "replaceWith": " mon wati "},
+  {"name": " ma ", "replaceWith": " ma wati "},
+  {"name": " mes ", "replaceWith": " mes watis "},
+  {"name": " ton ", "replaceWith": " ton wati "},
+  {"name": " ta ", "replaceWith": " ta wati "},
+  {"name": " tes ", "replaceWith": " tes watis "},
+  {"name": " son ", "replaceWith": " son wati "},
+  { "name": " sa ", "replaceWith": " sa wati "},
+  {"name": " ses ", "replaceWith": " ses watis "},
+  {"name": " notre ", "replaceWith": " notre wati "},
+  {"name": " nos ", "replaceWith": " nos watis "},
+  {"name": " votre ", "replaceWith": " votre wati "},
+  {"name": " vos ", "replaceWith": " vos watis "},
+  {"name": " leur ", "replaceWith": " leur watis "},
+  {"name": " leurs ", "replaceWith": " leurs watis "},
+
+  {"name": "Mon ", "replaceWith": "Mon wati "},
+  {"name": "Ma ", "replaceWith": "Ma wati "},
+  {"name": "Mes ", "replaceWith": "Mes watis "},
+  {"name": "Ton ", "replaceWith": "Ton wati "},
+  {"name": "Ta ", "replaceWith": "Ta wati "},
+  {"name": "Tes ", "replaceWith": "Tes watis "},
+  {"name": "Son ", "replaceWith": "Son wati "},
+  {"name": "Sa ", "replaceWith": "Sa wati "},
+  {"name": "Ses ", "replaceWith": "Ses watis "},
+  {"name": "Notre ", "replaceWith": "Notre wati "},
+  {"name": "Nos ", "replaceWith": "Nos watis "},
+  {"name": "Votre ", "replaceWith": "Votre wati "},
+  {"name": "Vos ", "replaceWith": "Vos watis "},
+  {"name": "Leur ", "replaceWith": "Leur watis "},
+  {"name": "Leurs ", "replaceWith": "Leurs watis "},
+  // démonstratifs
+  {"name": " ce ", "replaceWith": " ce wati "},
+  {"name": " cette ", "replaceWith": " cette wati "},
+  {"name": " cet ", "replaceWith": " cet wati "},
+  {"name": " ces ", "replaceWith": " ces watis "},
+
+  {"name": "Ce ", "replaceWith": "Ce wati "},
+  {"name": "Cette ", "replaceWith": "Cette wati "},
+  {"name": "Cet ", "replaceWith": "Cet wati "},
+  {"name": "Ces ", "replaceWith": "Ces watis "},
+  // autres
+  {"name": " c'est ", "replaceWith": " c'est wati "},
+  {"name": " du ", "replaceWith": " du wati "},
+
+  {"name": "C'est ", "replaceWith": "C'est wati "},
+  {"name": "Du ", "replaceWith": "Du wati "},
 ];
 /* For copy/paste
   {
